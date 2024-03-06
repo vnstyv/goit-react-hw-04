2dc49079a2218adb9e3ba5839820850566e6c96a
Anastasia Romanyuk <vnstyv@gmail.com>
2024-03-07 01:02:07 +0200

    ed6

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import "modern-normalize";
-import { App } from "../../components/App/App.jsx";
+import 'modern-normalize';
+import { App } from './components/App/App';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
