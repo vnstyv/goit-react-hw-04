b407f9753236cae54908d034307ecdc7738eedc5
Anastasia Romanyuk <vnstyv@gmail.com>
2024-03-07 00:44:42 +0200

    ed3

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,29 +1,32 @@
+import css from "./ImageModal.module.css";
+import { LiaHandPointer } from "react-icons/lia";
+
 export default function ImageModal({
-    content: {
-        likes,
-        alt_description,
-        urls: { regular, raw },
-        breadcrumbs,
-    },
+  content: {
+    likes,
+    alt_description,
+    urls: { regular, raw },
+    breadcrumbs,
+  },
 }) {
-    const tags = breadcrumbs.map((item) => item.slug).join(', ');
-    return (
-        <>
-            <img className={css.img} src={regular} alt={alt_description} />
-            <div className={css.info}>
-                <p>Likes: {likes}</p>
-                <p>Tags: {tags}</p>
-                <a
-                    className={css.link}
-                    href={raw}
-                    download={`${alt_description}.jpg`}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                >
-                    <LiaHandPointer className={css.icon} />
-                    High-resolution image, available for download
-                </a>
-            </div>
-        </>
-    );
+  const tags = breadcrumbs.map((item) => item.slug).join(", ");
+  return (
+    <>
+      <img className={css.img} src={regular} alt={alt_description} />
+      <div className={css.info}>
+        <p>Likes: {likes}</p>
+        <p>Tags: {tags}</p>
+        <a
+          className={css.link}
+          href={raw}
+          download={`${alt_description}.jpg`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LiaHandPointer className={css.icon} />
+          Raw pic, ready to download
+        </a>
+      </div>
+    </>
+  );
 }
